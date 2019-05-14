2a9e94820d0fb20e9c0779aef79b62523794c4b1
Leon <leon.id@qq.com>
2019-05-14 15:42:27 +0800

    支持处理TXT文件；

--- OracleSQLExecuterBuilder/Program.cs
+++ OracleSQLExecuterBuilder/Program.cs
@@ -18,6 +18,7 @@ namespace OracleSQLExecuterBuilder
                 .Select(sqlfile => new SQLFile(sqlfile.Substring(currentDirLength)))
                 .Where(sqlFile =>
                         sqlFile.Extention == ".SQL" ||
+                        sqlFile.Extention == ".TXT" ||
                         sqlFile.Extention == ".PCK" ||
                         sqlFile.Extention == ".PRC" ||
                         sqlFile.Extention == ".SPC" ||
